0906a888879906b1d5f4f6ae275317d4602fe03b
isaacaflores2 <isaac.a.flores2@gmail.com>
2019-11-21 05:56:03 -0800

    Added an examples folder with a the Person class, two small programs to read and write, and an example spreadsheet to read. The examples are continuation of the original readme examples.

---
+++ examples/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Excel.IO.examples
+{
+    public class Person : IExcelRow
+    {
+        public string SheetName { get => "People Sheet"; }
+
+        public string EyeColour { get; set; }
+
+        public int Age { get; set; }
+
+        public int Height { get; set; }
+    }
+}
---
+++ examples/WriteExample.cs
@@ -0,0 +1,26 @@
+﻿using Excel.IO;
+using System;
+using System.Collections.Generic;
+
+namespace Excel.IO.examples
+{
+    class WriteExample
+    {
+        static void Main(string[] args)
+        {                          
+            var people = new List<Person>();
+            
+            for (int i = 0; i < 10; i++)
+            {
+                people.Add(new Person
+                {
+                    EyeColour = Guid.NewGuid().ToString(),
+                    Age = new Random().Next(1, 100),
+                    Height = new Random().Next(100, 200)
+                });
+            }
+            var excelConverter = new ExcelConverter();
+            excelConverter.Write(people, "C:\\somefolder\\people.xlsx");                      
+        }
+    }  
+}
